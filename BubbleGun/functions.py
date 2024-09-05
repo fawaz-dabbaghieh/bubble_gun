53b8d68d0c9d0c35252da4dc1b4bbb6af57b4631
Fawaz Dabbaghie <fawaz.dabbaghieh@gmail.com>
2024-09-05 22:34:09 +0900

    fixing a bug where compacting breaks if there is * instead of a sequence

--- BubbleGun/functions.py
+++ BubbleGun/functions.py
@@ -7,7 +7,7 @@ def current_time():
 
 
 def reverse_complement(dna):
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N':'N'}
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N':'N', "*":"*"}
     return ''.join([complement[base] for base in dna[::-1]])
 
 
